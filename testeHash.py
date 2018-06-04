1c4dc3a0f734867a30e01729b758d97bdfa81bda
rico2290 <rjl220990@gmail.com>
2018-06-03 23:32:03 -0300

    versão de teste

--- testeHash.py
+++ testeHash.py
@@ -1,27 +1,88 @@
-#import pandas as pd
+import pandas as pd
 #import numpy as np
 import csv
 
-#with open('customer.tbl') as file:
-#tabela = pd.read_table(file, sep='|', index_col=0, header=None, lineterminator='\n', names=['id','name','address','nationkey','phone','acctbal','mktsegment','comment'])
-#saida = open('result.csv','w')
-#tabela.to_csv(saida,index=None)
+'''
+with open('orders.tbl') as file:
+    tabela = pd.read_table(file, sep='|', index_col=0, header=None, lineterminator='\n', 
+                        names=['orderKey','custKey','orderStatus','totalPrice','orderDate','orderPriority',
+                        'clerk','shipPriority', 'comment'])
+    saida = open('order.csv','w')
+    tabela.to_csv(saida,index=None) '''
+
+'''
+def transform(idCol):
+     #Se nao atingir o tamanho permitido
+     if len(idCol) <= 12:
+        t = 12-len(idCol)
+        t = '{:0>12}'.format(idCol)
+        print(t)
+       
+     #Se passar do tamanho permitido
+     if len(idCol) > 12:
+        t = len(idCol)-12
+        t = idCol[t:]
+        print(t)
+        #t = '{:0>12}'.format(idCol) 
+        #print(t)
+   
+     
 x = 20
 cont = 0
-with open('result.csv') as csvfile:
-    global pegaId       
+with open('order.csv') as csvfile:      
     read = csv.reader(csvfile)
     for row in read:
-        if cont <= x:
-            if row[0] == 'id': continue
-            pageId = row[0]
-            
-            cont = cont +1
-            pageId = ("{0:b}".format(int(pageId[12:])))
-            #print(type(pageId)
-            #caso o tamanho do ID for menor que o exigido
-            if len(pageId) <= 5:
-                a = 5-len(pageId)
-                a ='{:0>5}'.format(pageId)
-                print(a)
-                
+        if cont < x:
+            cont = cont+1
+            print(row)
+print('################################################################################')
+
+x = 20
+cont = 0
+with open('customer.csv') as csvfile:   
+    read = csv.reader(csvfile)
+    for row in read:
+        if cont < x:
+            cont = cont+1
+            print(row)
+
+
+transform('110101')
+
+'''
+def transform(idCol):
+     contador = 0
+     t = ' '
+     #Se nao atingir o tamanho exigido
+     if len(idCol) <= 12:
+        t = 12-len(idCol)
+        t = '{:0>12}'.format(idCol)
+        print(t)
+        
+       
+     #Se passar do tamanho exigido
+     if len(idCol) > 12:
+        t = len(idCol)-12
+        t = idCol[t:]
+        print(t)
+        #t = '{:0>12}'.format(idCol) 
+       
+     
+
+
+
+
+#x = 200
+#cont = 0
+with open('customer.csv') as csvfile:
+     pegaId = ' '   
+     s = 0 #confirmar qtde d linhas impressas    
+     read = csv.reader(csvfile)
+     for row in read:
+       
+        if row[0] == 'id': 
+            continue
+        pageId = row[0]
+        s = s+1
+        pageId = ("{0:b}".format(int(pageId[12:])))
+        print(type(pageId)
